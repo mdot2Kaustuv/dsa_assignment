03fb97ed5e843ed05369c24eb573a506e17c01ca
codeKGB <kbustuv@gmail.com>
2026-02-10 16:46:28 +0545

    Documentation

--- Programs/prog_7.c
+++ Programs/prog_7.c
@@ -24,8 +24,8 @@ void heapifyMin(int arr[], int n, int i)
 void heapifyMax(int arr[], int n, int i)
 {
     int largest = i;
-    int left = 2 * i + 1;
     int right = 2 * i + 2;
+     int left = 2 * i + 1;
 
     if (left < n && arr[left] > arr[largest])
         largest = left;
